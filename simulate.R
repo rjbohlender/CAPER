d01f07ac348491ecb7225009e866e37112c8a287
Bohlender <rjbohlender@mdanderson.org>
2018-10-21 21:44:09 -0500

    Quick simulation script for testing.

---
+++ simulate.R
@@ -0,0 +1,83 @@
+# Quick and dirty genetic simulation
+
+sink("~/Desktop/test.matrix")
+total <- 1000000
+
+# Log odds 
+lodds <- log(10)
+
+ngenes <- 20
+nsnps <- rpois(ngenes, 20)
+ncausal <- 10
+
+freq <- list()
+for(i in 1:ngenes) {
+  freq[[i]] <- runif(nsnps[i], 5 / total, 65 / total)
+}
+
+
+data = NULL
+location = 0
+for(i in 1:ngenes) {
+  gene = NULL
+  for(j in 1:nsnps[i]) {
+    location = location + 1
+    gene = rbind(gene, rbinom(total, 2, freq[[i]][j]))
+    cat(
+      paste(paste0("gene_", i), 
+            paste0("transcript_", i), 
+            paste("chr1", location, location, sep="-"),
+            paste(gene[j,], sep = "", collapse = '\t'),
+            "\n",
+            sep = "\t"))
+  }
+  data = cbind(data, colSums(gene))
+}
+
+sink()
+
+betas <- c(rep(lodds, ncausal), rep(log(0.1) / (ngenes - ncausal), ngenes - ncausal))
+case_control <- rbinom(total, 1, binomial()$linkinv(data %*% betas))
+
+case_count = 0;
+control_count = 0;
+
+labels = NULL
+for(v in case_control) {
+  if(v == 1) {
+    case_count = case_count + 1
+    labels <- c(labels, sprintf("case_%g", case_count))
+  } else {
+    control_count = control_count + 1
+    labels <- c(labels, sprintf("control_%g", control_count))
+  }
+}
+header <- paste("#Gene\tTranscript\tLocation", paste(labels, collapse = "\t"), sep="\t")
+
+# Output the matrix header
+
+sink("~/Desktop/header.txt")
+cat(header)
+sink()
+
+# Write a ped file
+
+sink("~/Desktop/test_data.ped")
+cat("#fid\tiid\tpid\tmid\tsex\taff\n")
+for(i in 1:length(labels)) {
+  if(case_control[i] == 1) {
+    cat(0, labels[i], 0, 0, 0, 2, sep = "\t")
+  } else {
+    cat(0, labels[i], 0, 0, 0, 1, sep = "\t")
+  }
+  cat("\n")
+}
+sink()
+
+# Write a covariate matrix
+
+sink("~/Desktop/test_data.cov")
+for(i in 1:length(labels)) {
+  cat(paste(labels[i], paste(rnorm(10), collapse="\t"), "\n", sep="\t"))
+}
+sink()
