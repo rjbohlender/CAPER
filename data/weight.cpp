06ef77098326f3dbf32aa23d3ab2e9d5ae5f18b8
Ryan Bohlender <ryan.bohlender@gmail.com>
2022-08-22 11:19:47 -0500

    Fix weight file formatting

--- data/weight.cpp
+++ data/weight.cpp
@@ -2,10 +2,10 @@
 // Created by Bohlender,Ryan James on 8/21/18.
 //
 
-#include <boost/algorithm/string/predicate.hpp>
 #include "weight.hpp"
 #include "../utility/filesystem.hpp"
 #include "../utility/filevalidator.hpp"
+#include <boost/algorithm/string/predicate.hpp>
 
 Weight::Weight(const std::string &ifile) {
   if (!check_file_exists(ifile)) {
@@ -18,99 +18,109 @@ Weight::Weight(const std::string &ifile) {
   FileValidator fileValidator;
 
   while (std::getline(ifs, line)) {
-	lineno++;
-    if(line.empty() || boost::starts_with(line, "#")) {
+    lineno++;
+    if (line.empty() || boost::starts_with(line, "#")) {
       continue;
     }
 
-	RJBUtil::Splitter<std::string> splitter(line, "\t");
-	fileValidator.validate_weight_line(splitter, lineno);
+    RJBUtil::Splitter<std::string> splitter(line, "\t");
+    fileValidator.validate_weight_line(splitter, lineno);
 
-	if(splitter.size() < 8) {
-	  std::cerr << "Incorrectly formatted weight line. Line was: " << line << std::endl;
-	  std::cerr << "Line should be tab separated and formatted as <chrom> <start_pos> <end_pos> <type> <weight>" << std::endl;
-	  throw(std::runtime_error("Incorrect line in weight file."));
+    if (splitter.size() < 8) {
+      std::cerr << "Incorrectly formatted weight line. Line was: " << line
+                << std::endl;
+      std::cerr << "Line should be tab separated and formatted as <chrom> "
+                   "<start_pos> <end_pos> <type> <weight>"
+                << std::endl;
+      throw(std::runtime_error("Incorrect line in weight file."));
     }
 
-	std::stringstream ss;
-// chr13   114326148       114326148       G       A       SNV     CHAMP1  NM_032436       2.55051363
-    if(splitter[5] == "SNP") {
-	  ss << splitter[0] << "," << splitter[1] << "," << splitter[2] << "," << splitter[3] << "," << splitter[4] << ",SNV," << splitter[6] << "," << splitter[7];
+    std::stringstream ss;
+    // chr13   114326148       114326148       G       A       SNV     CHAMP1
+    // NM_032436       2.55051363
+    if (splitter[5] == "SNP") {
+      ss << splitter[0] << "," << splitter[1] << "," << splitter[2] << ","
+         << splitter[3] << "," << splitter[4] << ",SNV," << splitter[6] << ","
+         << splitter[7];
     } else {
-          ss << splitter[0] << "," << splitter[1] << "," << splitter[2] << "," << splitter[3] << "," << splitter[4] << "," << splitter[5] << "," << splitter[6] << "," << splitter[7];
+      ss << splitter[0] << "," << splitter[1] << "," << splitter[2] << ","
+         << splitter[3] << "," << splitter[4] << "," << splitter[5] << ","
+         << splitter[6] << "," << splitter[7];
     }
 
+    double score;
+    try {
+      score = std::stod(splitter.back());
+    } catch (std::exception &e) {
+      std::cerr << "Failed to convert weight to double. Line was: " << line
+                << std::endl;
+      std::cerr << "Line should be tab separated and formatted as <chrom> "
+                   "<start_pos> <end_pos> <type> <weight>"
+                << std::endl;
+      throw(e);
+    }
 
-	double score;
-	try {
-		score = std::stod(splitter.back());
-	} catch(std::exception &e) {
-		std::cerr << "Failed to convert weight to double. Line was: " << line << std::endl;
-		std::cerr << "Line should be tab separated and formatted as <chrom> <start_pos> <end_pos> <type> <weight>" << std::endl;
-		throw(e);
-	}
-
-	// Prevent math errors
-	scores_[ss.str()] = score;
+    // Prevent math errors
+    scores_[ss.str()] = score;
   }
 }
 
 double Weight::get(const std::string &k) const {
-  try{
-	return scores_.at(k);
-  } catch(std::exception &e) {
+  try {
+    return scores_.at(k);
+  } catch (std::exception &e) {
     std::cerr << "Failed to find weight: " << k << std::endl;
     return 1;
   }
 }
 
-double Weight::get(const std::string &k) {
-  return scores_.at(k);
-}
+double Weight::get(const std::string &k) { return scores_.at(k); }
 
-bool Weight::empty() const {
-  return scores_.empty();
-}
+bool Weight::empty() const { return scores_.empty(); }
 
-bool Weight::empty() {
-  return scores_.empty();
-}
+bool Weight::empty() { return scores_.empty(); }
 
 Weight::Weight(std::stringstream &ifile) {
   std::string line;
 
   while (std::getline(ifile, line, '\n')) {
-	if(line.empty() || line[0] == '#') {
-	  continue;
-	}
-	RJBUtil::Splitter<std::string> splitter(line, "\t");
+    if (line.empty() || line[0] == '#') {
+      continue;
+    }
+    RJBUtil::Splitter<std::string> splitter(line, "\t");
 
-	if(splitter.size() < 5) {
-	  std::cerr << "Incorrectly formatted weight line. Line was: " << line << std::endl;
-	  std::cerr << "Line should be tab separated and formatted as <chrom> <start_pos> <end_pos> <type> <weight>" << std::endl;
-	  throw(std::runtime_error("Incorrect line in weight file."));
-	}
+    if (splitter.size() < 5) {
+      std::cerr << "Incorrectly formatted weight line. Line was: " << line
+                << std::endl;
+      std::cerr << "Line should be tab separated and formatted as <chrom> "
+                   "<start_pos> <end_pos> <type> <weight>"
+                << std::endl;
+      throw(std::runtime_error("Incorrect line in weight file."));
+    }
 
-	std::stringstream ss;
+    std::stringstream ss;
 
-	if(splitter[3] == "SNP") {
-	  ss << splitter[0] << "-" << splitter[1] << "-" << splitter[2] << "-" << "SNV";
-	} else {
-	  ss << splitter[0] << "-" << splitter[1] << "-" << splitter[2] << "-" << splitter[3];
-	}
+    if (splitter[3] == "SNP") {
+      ss << splitter[0] << "-" << splitter[1] << "-" << splitter[2] << "-"
+         << "SNV";
+    } else {
+      ss << splitter[0] << "-" << splitter[1] << "-" << splitter[2] << "-"
+         << splitter[3];
+    }
 
+    double score;
+    try {
+      score = std::stod(splitter[4]);
+    } catch (std::exception &e) {
+      std::cerr << "Failed to convert weight to double. Line was: " << line
+                << std::endl;
+      std::cerr << "Line should be tab separated and formatted as <chrom> "
+                   "<start_pos> <end_pos> <type> <weight>"
+                << std::endl;
+      throw(e);
+    }
 
-	double score;
-	try {
-	  score = std::stod(splitter[4]);
-	} catch(std::exception &e) {
-	  std::cerr << "Failed to convert weight to double. Line was: " << line << std::endl;
-	  std::cerr << "Line should be tab separated and formatted as <chrom> <start_pos> <end_pos> <type> <weight>" << std::endl;
-	  throw(e);
-	}
-
-	// Prevent math errors
-	scores_[ss.str()] = score;
+    // Prevent math errors
+    scores_[ss.str()] = score;
   }
 }
-
